827ca291bea1fa5b255ba28721413359a037846a
Mark <52928930+Mark4Titan@users.noreply.github.com>
2023-05-10 09:45:19 +0300

    add placeholder

--- src/components/ico/Icons.js
+++ src/components/ico/Icons.js
@@ -13,29 +13,63 @@ import { HiDatabase } from "react-icons/hi";
 import styled from "@emotion/styled";
 
 const ICO = {
-  edit: RiBallPenLine,
-  burgrtClose: RiLayout5Line,
-  burgrtOpen: RiLayoutTopLine,
-  add: RiFileAddLine,
-  del: RiFileForbidLine,
+  edit: { i: RiBallPenLine, text: "edit" },
+  burgrtClose: { i: RiLayout5Line, text: "hide" },
+  burgrtOpen: { i: RiLayoutTopLine, text: "show" },
+  add: { i: RiFileAddLine, text: "add" },
+  del: { i: RiFileForbidLine, text: "remove" },
 
-  settings: RiSoundModuleLine,
+  settings: { i: RiSoundModuleLine, text: "settings" },
 
-  cloud: RiSoundcloudLine,
-  Databas: HiDatabase,
+  cloud: { i: RiSoundcloudLine, text: "quintadb" },
+  Databas: { i: HiDatabase, text: "indexeddb" },
 
-  rec: RiSave3Fill,
-  search: BiSearch,
+  rec: { i: RiSave3Fill, text: "record" },
+  search: { i: BiSearch, text: "search" },
 };
 
 const Icons = ({ ico, C }) => {
-  return <C>{ICO[ico]()}</C>;
+  const text = ICO[ico].text;
+  const Ico = ICO[ico].i();
+  return <C data-text={text}>{Ico}</C>;
 };
 
 export default Icons;
 
 //
+
+const StyBefore = `  
+
+  &::before{
+    top: -25px;    
+    position: absolute;
+    border-radius: 5px;
+    background-color: #fafaf8;
+    
+    border-color: #d6d6d6;
+    content: " ";
+    box-shadow: 0px 3px 8px 0px #4f4f4f5e;
+    align-items: center;
+    justify-items: center;
+    justify-content: center;
+    opacity: 0;
+    display: grid;
+    transition: all 0.1s ease-out;
+  }
+  &:hover::before{
+    top: -45px;
+    opacity: 1;
+    width: auto;
+    padding: 5px;
+    height: auto;
+    content: attr(data-text);
+   
+  }
+  
+`;
+
 export const StyBut = styled.div`
+  position: relative;
   width: 48px;
   height: 26px;
   border: solid 1px #d6d6d6;
@@ -56,6 +90,7 @@ export const StyBut = styled.div`
   &:active {
     background-color: #ffffff8c;
   }
+  ${StyBefore}
 `;
 export const StyIco = styled.div`
   position: absolute;
@@ -68,8 +103,10 @@ export const StyIco = styled.div`
     width: 20px;
     height: 20px;
   }
+  ${StyBefore}
 `;
 export const StyDatabasIn = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -80,8 +117,10 @@ export const StyDatabasIn = styled.div`
     height: 20px;
     color: darkgreen;
   }
+  ${StyBefore}
 `;
 export const StyDatabasOut = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -92,4 +131,5 @@ export const StyDatabasOut = styled.div`
     height: 20px;
     color: brown;
   }
+  ${StyBefore}
 `;
